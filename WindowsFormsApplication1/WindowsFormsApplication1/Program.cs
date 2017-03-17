72d50aa24d76e1cf80b2d12128f7a3ae97fe8247
Rasniko <markownikolic@hotmail.com>
2017-03-17 23:46:49 +0100

    Specifikacija zahtjeva dodana, i nesto sitno u kodu

--- WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
@@ -17,8 +17,6 @@ namespace WindowsFormsApplication1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Window());
-
-
         }
     }
 }
